1e4f407a711de9db3ebd9752f42e22f888ac3ce5
ZeframCochran <Anonygram@gmail.com>
2017-12-13 00:37:52 -0800

    First commit on my first Atom plugin!

---
+++ spec/toggle-case-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CapsLocCapitalizeView from '../lib/toggle-case-view';
+
+describe('CapsLocCapitalizeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
